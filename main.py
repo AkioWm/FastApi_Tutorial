7c99116e86dc6301513a57b041ec3c6ed282c4b1
AkioWm <cherif.kante16@gmail.com>
2025-11-18 17:09:47 +0100

    create my first get request with Fastapi !

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def main():
-    print("Hello from fastapi-tutorial!")
-
+import uvicorn
 
 if __name__ == "__main__":
-    main()
+    print("Hello World")
+    uvicorn.run("app.app:app", host="0.0.0.0", port=8000,reload=True)
 
