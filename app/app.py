ef1baf5b8bdeedde361909c167d1c28fa1a90cf9
AkioWm <cherif.kante16@gmail.com>
2025-11-20 18:30:23 +0100

    add get_feed function

--- app/app.py
+++ app/app.py
@@ -3,6 +3,7 @@ from app.schemas import PostCreate, PostResponse
 from app.db import Post, create_db_and_tables, get_async_session
 from sqlalchemy.ext.asyncio import AsyncSession
 from contextlib import asynccontextmanager
+from sqlalchemy import select
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -18,7 +19,7 @@ async def upload_file(
     file: UploadFile = File(...),
     caption: str = Form(""),
     session: AsyncSession = Depends(get_async_session)
-    ):
+):
     post = Post(
         caption = caption,
         url="someURL",
@@ -31,8 +32,28 @@ async def upload_file(
     return post 
     
 
-# @app.get("/posts")
-# def get_all_posts(limit: int = None):
+@app.get("/feed")
+async def get_feed(
+    session: AsyncSession = Depends(get_async_session)
+):
+    
+    result = await session.execute(select(Post).order_by(Post.create_at.desc()))
+    posts = [row[0] for row in result.all()]
+   
+    posts_data = []
+    for post in posts:
+        posts_data.append(
+            {
+               "id": str(post.id),
+               "caption": post.caption,
+               "url": post.url,
+               "file_type": post.file_type,
+               "file_name": post.file_name,
+               "create_at": post.create_at.isoformat()
+           }
+        )        
+    
+    return {"posts": posts_data}
 
 # @app.get("/posts/{id}")
 # def get_post(id: int) -> PostResponse:
