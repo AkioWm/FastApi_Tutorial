93ee2210e9d9543412a60f023fbe92e1ef66a39d
AkioWm <cherif.kante16@gmail.com>
2025-11-20 14:29:40 +0100

    add simple get post

--- app/app.py
+++ app/app.py
@@ -1,7 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
-@app.get("/hello-world")
-def hello_world():
-    return{"message":"Hello World"}
+
+text_posts = {1:{"title":"Nissan 400z","content":"the Nissan 400z is awesome!"}, 
+                 2:{"title":"Nissan 240z","content":"the Nissan 240z is a legend !"}}
+
+@app.get("/posts")
+def get_all_posts():
+    return text_posts
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    if id not in text_posts:
+        raise HTTPException(status_code=404,detail="Post not found")
+    return text_posts.get(id)
+
